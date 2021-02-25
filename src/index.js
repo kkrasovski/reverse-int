cddbc25bf31c1ed54e44c3417a3bb44a09da1988
Kiryl Krasouski <ziksow@gmail.com>
2021-02-25 09:36:48 +0300

    init: start reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    splitInput = String(n).split("");
+    let result;
+    if (splitInput[0] == "-") {
+        splitInput.shift("");
+        result = splitInput.reverse().join("");
+    } else {
+        result = splitInput.reverse().join("");
+    }
+    return result;
+};
